18c4fe6b1173102094025c84024e45a7ee145e0b
bsahlas <bill.sahlas@gmail.com`>
2025-08-08 15:21:43 -0400

    Fix JS errors.

--- www/src/App.js
+++ www/src/App.js
@@ -1,23 +1,29 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-
-const AllureReportViewer = () => {
-  return (
-    <div style={{ width: '100%', height: '800px' }}>
-      <iframe
-        src="https://sahlas.github.io/swag-labs-java-playwright-cucumber/allure-report/index.html"
-        title="Allure Report"
-        style={{ width: '100%', height: '100%', border: 'none' }}
-      />
-    </div>
-  );
-};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <AllureReportViewer />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+            <div style={{ width: '100%', height: '800px' }}>
+              <iframe
+                src="https://sahlas.github.io/swag-labs-java-playwright-cucumber/allure-report/index.html"
+                title="Allure Report"
+                style={{ width: '100%', height: '100%', border: 'none' }}
+              />
+            </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
