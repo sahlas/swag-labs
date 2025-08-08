853c9e367957f5a1e59e703cb779b61305b3b683
bsahlas <bill.sahlas@gmail.com`>
2025-08-08 15:04:13 -0400

    Fix JS errors.

--- www/src/App.js
+++ www/src/App.js
@@ -2,25 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+const AllureReportViewer = () => {
+  return (
+    <div style={{ width: '100%', height: '800px' }}>
+      <iframe
+        src="https://sahlas.github.io/swag-labs-java-playwright-cucumber/allure-report/index.html"
+        title="Allure Report"
+        style={{ width: '100%', height: '100%', border: 'none' }}
+      />
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-
-    const AllureReportViewer = () => {
-      return (
-        <div style={{ width: '100%', height: '800px' }}>
-          <iframe
-            src="https://sahlas.github.io/swag-labs-java-playwright-cucumber/allure-report/index.html" // Adjust this path based on where you host the report
-            title="Allure Report"
-            style={{ width: '100%', height: '100%', border: 'none' }}
-          />
-        </div>
-      );
-    };
-
-    export default AllureReportViewer;
+        <AllureReportViewer />
       </header>
     </div>
   );
